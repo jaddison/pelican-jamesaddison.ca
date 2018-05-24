d719c2f3c9f433fb54cd54261be5f9ab3f650374
James Addison <addi00+github.com@gmail.com>
2018-05-24 13:05:21 -0700

    Adding email address. Removing site name from all page titles but the homepage.

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-RELATIVE_URLS = False
-
 OUTPUT_PATH = '../jaddison.github.io'
+
+RELATIVE_URLS = False
+DELETE_OUTPUT_DIRECTORY = False
